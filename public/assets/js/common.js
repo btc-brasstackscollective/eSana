fb4eba85f203798b9226a8d4c00c6ce0e7726972
Doug Ingalls <Doug.Ingalls@Group1200.com>
2017-10-02 02:29:59 -0500

    global template updates and home page updates

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -4,5 +4,22 @@
 		// Home Page Hero Panel
 		if($('#home_hero_container').length)
 			$('#home_hero_container').customPanel();
+			
+		// Mobile Navigation :: Setup
+		$('#main_navigation').after($('<div id="navbar-height-col"></div>'));
+	
+	    // Enter your ids or classes
+	    var toggler = '.navbar-toggle';
+	    var navigationwrapper = '.navbar-header';
+	
+		$("#main_navigation").on("click load", toggler, function (e)
+		{
+	        var selected = $(this).hasClass('slide-active');
+	
+	    	$(this).toggleClass('slide-active', !selected);
+	    	$('#navbar-height-col, #utility_nav').toggleClass('slide-active');
+	
+	    	$(navigationwrapper+', .navbar, body').toggleClass('slide-active');
+		});
 	});
 })( jQuery );
